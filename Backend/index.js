328ad0e37619619b10e4a14d7bbca866f4e0c3df
Enyew <enyewanb0000@gmail.com>
2023-10-24 10:36:46 +0300

    the API for the signup and Signin route is created

--- Backend/index.js
+++ Backend/index.js
@@ -28,3 +28,12 @@ app.listen(port, () =>{
 
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
+
+app.use((err, req,res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })})
