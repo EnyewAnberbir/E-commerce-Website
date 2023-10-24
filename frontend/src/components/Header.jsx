b6f618f7cdfef607e7de325f7a8c82a2d39707c5
natnaelyimer2040 <natnaelyimer2040@gmail.com>
2023-10-24 18:11:56 +0100

    update the header and make the profile page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import { FaSearch } from 'react-icons/fa';
 import { Link} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>      
@@ -14,7 +18,17 @@ export default function Header() {
          <FaSearch className="text-slate-600" />  </form>
          <ul className='flex gap-4'><Link to='/'> <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
          <Link to='/about'><li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
-         <Link to='/Sign-in'><li className=' text-slate-700 hover:underline'>{' '}Sign in</li></Link>
+         <Link to='/profile'>
+            {currentUser ? (
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
+          </Link>
          </ul></div>
 </header>
   )
